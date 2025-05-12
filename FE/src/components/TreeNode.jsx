1c262da47b39a8604fd5ca4f4b234a2369553838
guntarahmbl <guntarahambali3@gmail.com>
2025-05-12 10:57:38 +0700

    fix: children name

--- FE/src/components/TreeNode.jsx
+++ FE/src/components/TreeNode.jsx
@@ -21,21 +21,21 @@ function TreeNode({ node }) {
         </div>
       </div>
 
-      {(node.children1?.length > 0 || node.children2?.length > 0) && (
+      {(node.Children1?.length > 0 || node.Children2?.length > 0) && (
         <div className="tree-children-wrapper">
             {/* Children1 - Left Side */}
-            {node.children1?.length > 0 && (
+            {node.Children1?.length > 0 && (
             <div className="tree-children-left tree-children">
-                {node.children1.map((child, i) => (
+                {node.Children1.map((child, i) => (
                 <TreeNode key={`c1-${child.Item1.Name}-${i}`} node={child} />
                 ))}
             </div>
             )}
 
             {/* Children2 - Right Side */}
-            {node.children2?.length > 0 && (
+            {node.Children2?.length > 0 && (
             <div className="tree-children-right tree-children">
-                {node.children2.map((child, i) => (
+                {node.Children2.map((child, i) => (
                 <TreeNode key={`c2-${child.Item2.Name}-${i}`} node={child} />
                 ))}
             </div>
@@ -49,9 +49,9 @@ function TreeNode({ node }) {
 TreeNode.propTypes = {
   node: PropTypes.shape({
     Item1: PropTypes.object.isRequired,
-    children1: PropTypes.array,
+    Children1: PropTypes.array,
     Item2: PropTypes.object.isRequired,
-    children2: PropTypes.array,
+    Children2: PropTypes.array,
   }).isRequired,
 };
 
