5a79a1e3812cf3e60440c62397333f3b633e5891
FityatulhaqRosyidi <fityatulhaqrosyidi25@gmail.com>
2025-05-13 11:07:35 +0700

    feat : add time & count

--- Main.go
+++ Main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"github.com/guntarahmbl/Tubes2_APAAJAUDAH/BE/Utils"
 	"fmt"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"os"
-	"strconv"
+	// "github.com/gin-contrib/cors"
+	// "github.com/gin-gonic/gin"
+	// "net/http"
+	// "os"
+	// "strconv"
 )
 
 func Backend(target string, algorithm string, maxRecipe int) {
@@ -20,48 +20,51 @@ func Backend(target string, algorithm string, maxRecipe int) {
 	} else {
 		mode = 1;
 	}
-	recipes, err := utils.GetRecipes(target, mode, maxRecipe)
+	recipes, time, nodeCount, err := utils.GetRecipes(target, mode, maxRecipe)
 	if err != nil {
 		log.Fatalf("Failed to read recipes: %v", err)
 	}
 
-	utils.PrintListOfTree(recipes)
+	// utils.PrintListOfTree(recipes)
 	fmt.Printf("Recipes found : %d\n", len(recipes))
+	fmt.Printf("Execution time : %f\n", time)
+	fmt.Printf("Node Visited : %d\n", nodeCount)
 
 	savePath := "data/recipes.json"
-	utils.SaveRecipes(recipes, savePath)
+	utils.SaveRecipes(recipes, time, nodeCount, savePath)
 }
 
 func main() {
-	// Backend("Alcohol","dfs",1000)
 
-	router := gin.Default()
+	Backend("Sun","bfs",4)
 
-	router.Use(cors.Default()) // mengizinkan semua origin
+	// router := gin.Default()
 
-	router.GET("/api/recipes", func(c *gin.Context) {
-		target := c.Query("target")      
-		algorithm := c.Query("algorithm")      
-		maxRecipe := c.Query("maxRecipe")    
+	// router.Use(cors.Default()) // mengizinkan semua origin
+
+	// router.GET("/api/recipes", func(c *gin.Context) {
+	// 	target := c.Query("target")      
+	// 	algorithm := c.Query("algorithm")      
+	// 	maxRecipe := c.Query("maxRecipe")    
 	
-		// Konversi cukup yang maxRecipe saja karena dari fe algorithm tuh "bfs" dan "dfs"
-		maxRecipeInt, _ := strconv.Atoi(maxRecipe)
+	// 	// Konversi cukup yang maxRecipe saja karena dari fe algorithm tuh "bfs" dan "dfs"
+	// 	maxRecipeInt, _ := strconv.Atoi(maxRecipe)
 
-		Backend(target,algorithm,maxRecipeInt)
+	// 	Backend(target,algorithm,maxRecipeInt)
 	
-		// Baca isi file recipes.json (Rencananya gausah write file)
-		data, err := os.ReadFile("data/recipes.json")
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membaca file"})
-			return
-		}
+	// 	// Baca isi file recipes.json (Rencananya gausah write file)
+	// 	data, err := os.ReadFile("data/recipes.json")
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membaca file"})
+	// 		return
+	// 	}
 	
-		// Kirim isi JSON-nya ke frontend (Rencananya langsung kirim)
-		c.Data(http.StatusOK, "application/json", data)
-	})
+	// 	// Kirim isi JSON-nya ke frontend (Rencananya langsung kirim)
+	// 	c.Data(http.StatusOK, "application/json", data)
+	// })
 
-	if err := router.Run(":8080"); err != nil {
-		log.Fatal("Failed to start server: ", err)
-	}
+	// if err := router.Run(":8080"); err != nil {
+	// 	log.Fatal("Failed to start server: ", err)
+	// }
 	
 }
