13e87d8f5c9c0e86fa1e4c51e8887f9304dabdf5
FityatulhaqRosyidi <fityatulhaqrosyidi25@gmail.com>
2025-05-13 11:14:32 +0700

    fix : lupa uncomment mainnya 😅

--- Main.go
+++ Main.go
@@ -4,11 +4,11 @@ import (
 	"log"
 	"github.com/guntarahmbl/Tubes2_APAAJAUDAH/BE/Utils"
 	"fmt"
-	// "github.com/gin-contrib/cors"
-	// "github.com/gin-gonic/gin"
-	// "net/http"
-	// "os"
-	// "strconv"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func Backend(target string, algorithm string, maxRecipe int) {
@@ -36,35 +36,35 @@ func Backend(target string, algorithm string, maxRecipe int) {
 
 func main() {
 
-	Backend("Sun","bfs",4)
+	// Backend("Sun","bfs",4)
 
-	// router := gin.Default()
+	router := gin.Default()
 
-	// router.Use(cors.Default()) // mengizinkan semua origin
+	router.Use(cors.Default()) // mengizinkan semua origin
 
-	// router.GET("/api/recipes", func(c *gin.Context) {
-	// 	target := c.Query("target")      
-	// 	algorithm := c.Query("algorithm")      
-	// 	maxRecipe := c.Query("maxRecipe")    
+	router.GET("/api/recipes", func(c *gin.Context) {
+		target := c.Query("target")      
+		algorithm := c.Query("algorithm")      
+		maxRecipe := c.Query("maxRecipe")    
 	
-	// 	// Konversi cukup yang maxRecipe saja karena dari fe algorithm tuh "bfs" dan "dfs"
-	// 	maxRecipeInt, _ := strconv.Atoi(maxRecipe)
+		// Konversi cukup yang maxRecipe saja karena dari fe algorithm tuh "bfs" dan "dfs"
+		maxRecipeInt, _ := strconv.Atoi(maxRecipe)
 
-	// 	Backend(target,algorithm,maxRecipeInt)
+		Backend(target,algorithm,maxRecipeInt)
 	
-	// 	// Baca isi file recipes.json (Rencananya gausah write file)
-	// 	data, err := os.ReadFile("data/recipes.json")
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membaca file"})
-	// 		return
-	// 	}
+		// Baca isi file recipes.json (Rencananya gausah write file)
+		data, err := os.ReadFile("data/recipes.json")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal membaca file"})
+			return
+		}
 	
-	// 	// Kirim isi JSON-nya ke frontend (Rencananya langsung kirim)
-	// 	c.Data(http.StatusOK, "application/json", data)
-	// })
+		// Kirim isi JSON-nya ke frontend (Rencananya langsung kirim)
+		c.Data(http.StatusOK, "application/json", data)
+	})
 
-	// if err := router.Run(":8080"); err != nil {
-	// 	log.Fatal("Failed to start server: ", err)
-	// }
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
 	
 }
